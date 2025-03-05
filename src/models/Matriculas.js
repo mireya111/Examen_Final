01399114d236441ab3bf1370dbbdf98215948c09
Mireya Josebeth García Rodríguez <151254789+mireya111@users.noreply.github.com>
2025-03-05 11:06:57 -0500

    Correccion en el codigo al registrar la matricula

--- src/models/Matriculas.js
+++ src/models/Matriculas.js
@@ -25,11 +25,12 @@ const matriculasEsquema = new Schema({
         timestamps: true
     }
 ); 
-matriculasEsquema.methods.codigoMatricula = async function(){
+matriculasEsquema.pre('save', function (next) {
     const fecha = new Date();
     const anio = fecha.getFullYear();
     const numeroDocumentos = Matriculas.find().countDocuments();
     const numeroConCeros = (numeroDocumentos + 1).toString().padStart(4, '0');
-    return `${anio}-${numeroConCeros}`;
-};
+    this. codigo =  `${anio}-${numeroConCeros}`;
+    next()
+});
 export default model('Matriculas', matriculasEsquema); 
